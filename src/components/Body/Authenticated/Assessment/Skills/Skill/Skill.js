03888db951db75b8decba957c716abff4c81485f
Johnny Magrippis <j@magrippis.com>
2017-02-12 11:00:51 +0000

    UI for showing skill descriptions

--- src/components/Body/Authenticated/Assessment/Skills/Skill/Skill.js
+++ src/components/Body/Authenticated/Assessment/Skills/Skill/Skill.js
@@ -1,11 +1,14 @@
 /* @flow */
 
 import React, { PureComponent } from 'react'
+import IconButton from 'material-ui/IconButton'
 import Paper from 'material-ui/Paper'
 import Slider from 'material-ui/Slider'
+import InfoOutline from 'material-ui/svg-icons/action/info-outline'
 import styled from 'styled-components'
 
 type Props = {
+  description?: string,
   id: string,
   name: string,
   setSkillValue: Function,
@@ -17,6 +20,11 @@ const Container = styled.div`
   text-transform: capitalize;
 `
 
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+`
+
 class Skill extends PureComponent {
   props: Props;
 
@@ -26,12 +34,21 @@ class Skill extends PureComponent {
   }
 
   render () {
-    const { name, value } = this.props
+    const { description, name, value } = this.props
 
     return (
       <Container>
         <Paper style={{ marginBottom: '1em', padding: '1em' }} zDepth={1}>
-          <div>{name}</div>
+          <Details>
+            {name}
+            {description
+              ? (
+                <IconButton tooltip={description} touch={true} tooltipPosition="top-right">
+                  <InfoOutline />
+                </IconButton>
+              )
+              : null}
+          </Details>
           <Slider
             defaultValue={0.5}
             name={name}
